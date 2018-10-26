4f27abaac966b27cf03092f0e3f2367b6d8f0a2b
remcovb <remcovanbogaert@icloud.com>
2018-10-26 11:35:30 +0200

    buttons mah dude

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
 {
-  document.querySelector(`.titel`).textContent = `Yep yep yep`;
+
+  const $title = document.querySelector('.title');
+
+  const init = () => {
+    console.log($title);
+    document.addEventListener('keydown', buttons);
+  }
+
+
+  const buttons = e => {
+    const keyName = e.key;
+
+    console
+
+    if (keyName === 'c') {
+    $title.innerHTML = "Bom"
+    return;
+    }
+
+    if (keyName === 'v') {
+    $title.innerHTML = "Banaan"
+    return;
+    }
+
+    if (keyName === 'b') {
+    $title.innerHTML = "Appelsien"
+    return;
+    }
+  }
+
+
+
+  init();
+
+
 }
